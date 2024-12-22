8033f91615573b4d21eb01fe637bc1f858730d59
LR0711 <lorenzo.rosselliilelli@studenti.unipd.it>
2024-12-22 18:59:34 +0100

    Creata classe soil come elemento base dell'ambiente agricolo

---
+++ soil.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+
+
+#include "soil.h"
+
+
+
+Soil::Soil() 
+    :soiltype_{SoilType::loam},
+    plants_{false},
+    soilmoisture_{50},
+    airtemperature_{20.0},
+    airhumidity_{50},
+    soiltemperature_{calculateSoilTemperature()}
+    {}
+
+Soil::Soil(SoilType soilType, bool plants, int soilMoisture, float airTemperature, int airHumidity)
+    :soiltype_{soilType},
+    plants_{plants},
+    soilmoisture_{soilMoisture},
+    airtemperature_{airTemperature},
+    airhumidity_{airHumidity},
+    soiltemperature_{calculateSoilTemperature()}
+    {
+        if (!ValidHumidity()) 
+        {
+            std::cerr << "Invalid humidity or moisture value. Humidity and moisture must be between 0 and 100." << std::endl;
+            exit(EXIT_FAILURE);
+
+        }
+    }
+
+    float Soil::calculateSoilTemperature()
+    {
+        switch(soiltype_)
+        {
+            case SoilType::clay:
+                return 0.6 * airtemperature_ + 0.006 * soilmoisture_ + 0.001 * airhumidity_ + 2.0;
+            case SoilType::sand:
+                return 0.8 * airtemperature_ + 0.0015 * soilmoisture_ + 0.0005 * airhumidity_ + 3.0;
+            case SoilType::loam:
+                return 0.7 * airtemperature_ + 0.002 * soilmoisture_ + 0.001 * airhumidity_ + 2.5;
+            case SoilType::silt:
+                return 0.65 * airtemperature_ + 0.0025 * soilmoisture_ + 0.001 * airhumidity_ + 2.0;
+            default:
+                cerr << "Invalid soil type." << endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+    bool Soil::ValidHumidity() const
+    {
+        if (airhumidity_ < 0 || airhumidity_ > 100 || soilmoisture_ < 0 || soilmoisture_ > 100) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void Soil::setSoilType(SoilType soilType)
+    {
+        soiltype_ = soilType;
+        soiltemperature_ = calculateSoilTemperature();
+    }   
+
+    void Soil::setPlants(bool plants)
+    {
+        plants_ = plants;
+    }
+    
+    void Soil::setSoilMoisture(int soilMoisture)
+    {
+        soilmoisture_ = soilMoisture;
+        soiltemperature_ = calculateSoilTemperature();
+    }
+
+    void Soil::setAirTemperature(float airTemperature)
+    {
+        airtemperature_ = airTemperature;
+        soiltemperature_ = calculateSoilTemperature();
+    }
+
+    void Soil::setAirHumidity(int airHumidity)
+    {
+        airhumidity_ = airHumidity;
+        soiltemperature_ = calculateSoilTemperature();
+    }
+
---
+++ soil.h
@@ -0,0 +1,43 @@
+// This class here is the "soil" class, that represents the base unit for the simulation of the agricultural field.
+// It's like a square portion of the bigger field, and it represents the idea that we can have different properties of the soil in the same field.
+// In the first version of this project, we consider that we can have different conditions in different areas of the field but this properties are static and hardcoded.
+// This class takes as input parameters an enum class representing 4 different type of possible soils (clay, sand, loam, silt), a bool saying if there are plants or not
+// an int representing the percentage of soil moisture and two int representing air temperature and air humidity in percentage.
+// There's another parameter, the soil temeperature, that is calculated by the class itself, based on the air temperature, the type of soil and the soil moisture.
+// The class gives method to get presence of plants and type of soil and to set all the parameters, but user can't get temeratures, moisture and humidity without a proper sensor class.
+
+#ifndef SOIL_H
+#define SOIL_H
+
+class Soil {
+    public:
+        enum class SoilType {clay, sand, loam, silt};
+        Soil();
+        Soil(SoilType soilType, bool plants, int soilMoisture, float airTemperature, int airHumidity);
+        void setSoilType(SoilType soilType);
+        void setPlants(bool plants);
+        void setSoilMoisture(int soilMoisture);
+        void setAirTemperature(float airTemperature);
+        void setAirHumidity(int airHumidity);
+        SoilType getSoilType() const {return soiltype_;}
+        bool getPlants() const {return plants_;}
+        
+
+
+
+    private:
+        SoilType soiltype_;
+        bool plants_;
+        int soilmoisture_;
+        float airtemperature_;
+        int airhumidity_;
+        float soiltemperature_;
+        float calculateSoilTemperature();
+        bool ValidHumidity () const;
+};
+
+
+
+
+
+#endif
