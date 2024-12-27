1222d31eccf7f7286b96ffc141b7e6069bdae726
LR0711 <lorenzo.rosselliilelli@studenti.unipd.it>
2024-12-27 23:02:47 +0100

    Creato costruttore e aggiunti metodi per leggere i dati dalla cella del campo

--- sensor.cpp
+++ sensor.cpp
@@ -4,10 +4,22 @@
 
 
 Sensor::Sensor()
-    :sensortype_{SensorType::TemperatureSensor}
+    :sensortype_{SensorType::AirTemperatureSensor}
     {}
 
 Sensor::Sensor(SensorType sensortype)
     :sensortype_{sensortype}
     {}
 
+float Sensor::readTemperature(const Soil& soil) const {
+    return soil.PassTemperatureToSensor(sensortype_);
+}
+
+int Sensor::readMoisture(const Soil& soil) const {
+    return soil.PassSoilMoistureToSensor(sensortype_);
+}
+
+int Sensor::readHumidity(const Soil& soil) const {
+    return soil.PassAirHumidityToSensor(sensortype_);
+}
+  
