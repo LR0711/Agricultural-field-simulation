620afe7d0ab6858a6c3416d57713660887ab76bf
LR0711 <lorenzo.rosselliilelli@studenti.unipd.it>
2024-12-29 16:42:44 +0100

    Aggiunto file di test per il movimento del veicolo all'interno del campo

---
+++ test_files/vehiclemovementtest.cpp
@@ -0,0 +1,27 @@
+#include "field.h"
+#include "vehicle.h"
+
+void testVehicleMovement()
+{
+    Field field("TestField", 10, 10);
+    Vehicle robot("TestVehicle", Vehicle::VehicleType::FieldVehicle, 0, 0, 1.0, 100.0, {}, field);
+    robot.setPosition(5, 5);
+    robot.moveToTarget(9, 7);
+    Vehicle drone("TestDrone", Vehicle::VehicleType::AerialVehicle, 0, 0, 2.0, 100.0, {}, field);
+    drone.setPosition(2, 9);
+    drone. moveToTarget(9, 2);
+    //Some test for errors checking: remove comment to see the error message
+    //Vehicle robot2("TestVehicle2", Vehicle::VehicleType::FieldVehicle, 0, 0, -1.0, 100.0, {}, field);
+    Vehicle robot3("TestVehicle3", Vehicle::VehicleType::FieldVehicle, -10, 0, 1.0, 100.0, {}, field);
+    Vehicle robot4("TestVehicle4", Vehicle::VehicleType::FieldVehicle, 10, 10, 1.0, 100.0, {}, field);
+}
+
+
+int main()
+{
+    testVehicleMovement();
+
+
+    return 0;
+    
+}
