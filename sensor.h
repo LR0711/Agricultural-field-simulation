ada88475e9bd10c790097db90e27a202c2aec68d
LR0711 <lorenzo.rosselliilelli@studenti.unipd.it>
2025-01-03 19:36:22 +0100

    Risolto errore sulla funzione di conversione in stringa e rimossa funzione sendData a causa di non utilizzo

--- sensor.h
+++ sensor.h
@@ -11,6 +11,7 @@
 
 #ifndef SENSOR_H
 #define SENSOR_H
+#include <string>
 
 class Soil;
 
@@ -25,7 +26,7 @@ class Sensor {
         int readHumidity(const Soil& soil) const;
         SensorType getType() const { return sensortype_; }
         static std::string sensorTypeToString(SensorType type);
-        void sendDatas();
+        
         
     private:
         SensorType sensortype_;
