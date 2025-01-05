6ed2fd1aba5bd6baa7c88a5c0daf6335cdd61d25
LR0711 <lorenzo.rosselliilelli@studenti.unipd.it>
2025-01-05 18:40:43 +0100

    Aggiornata la classe Vehicle per migliorare la documentazione e la chiarezza delle funzionalità, inclusa la distinzione tra veicoli terrestri e aerei, e la gestione della batteria durante le operazioni.

--- vehicle.h
+++ vehicle.h
@@ -1,12 +1,10 @@
-// The following class is the "vehicle" class, that represents the vehicles that can be used in the simulation of the agricultural field.
-// The class has a constructor that takes as input parameters the name of the vehicle, the type of the vehicle (field or aerial), the initial coordinates in the field, the vehicle speed and the battery level.
-// This vehicles will have on them also sensor objects, that will be used to get the temperature, humidity and moisture of the soil.
-// This class has the ability to move in the field class and collect datas from the soil celles.
-// In order to simulate a real vehicle, the vehicle does a navigation towards the target cell taking a time proportinal the distance between the current cell and the target cell and the speed of the vehicle.
-// The vehicle consumes battery in the movement, and the battery level is updated after the movement. If the battery level is too low, the vehicle can't move anymore and it has to wait for a recharge.
-// The vehicle can also recharge itself, but it can't move during the recharge.
-// Lastly, the vehicle has to communicate to a central unit his position and, when reached the target and get datas back from the sensor, the datas collected.
-// Sensors are built in another class.
+// La classe "Vehicle" rappresenta l'unità autonoma che si muove all'interno del campo per raccogliere i dati.
+// Ogni veicolo ha un nome, un id univoco, una posizione (x, y), una velocità, una batteria, un insieme di sensori e un riferimento al campo di operazione.
+// I veicoli possono essere di due tipi: veicoli terrestri e veicoli aerei. Nella versione attuale del progetto la scelta tra l'uno e l'altro tipo non comporta alcuna distinzione.
+// I veicoli possono muoversi all'interno del campo, leggere i dati dalla cella corrente e inviarli al control center.
+// Al fine di aggiungere aspetti di realtà alla simulazione, i veicoli scaricano la loro batteria eseguendo azioni, e al raggiungimento di una soglia critica sono "costretti" ad interrompere per del tempo le operazioni.
+// Ciò viene simulato con apposite funzioni spiegate nel file "vehicle.cpp".
+// Nel file sono inoltre inseriti descrizioni sintetiche delle funzioni.
 
 
 #ifndef VEHICLE_H
@@ -62,7 +60,7 @@ class Vehicle {
         std::vector<Sensor> sensors_;
         const Field& field_;
         bool isBusy_;
-        std::mutex mtx_;
+        std::mutex vehiclemutex_;
         std::condition_variable cvnotbusy_;
 
         
